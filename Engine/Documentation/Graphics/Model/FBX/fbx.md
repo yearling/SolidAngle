a23c93ea783993eab190e39b056ece3cb5ba1b6f
yearling <yearlingwes@gmail.com>
2017-09-06 01:51:51 +0800

    add fbx.md

--- Engine/Documentation/Graphics/Model/FBX/fbx.md
+++ Engine/Documentation/Graphics/Model/FBX/fbx.md
@@ -1,7 +1,7 @@
 # Fbx
 通用的模型交换格式
 
-# 关注点
+## 关注点
 1. 手系
 	1. 建模软件的手系：maya, max的手系 
 	2. 引擎的手系: gl,dx
@@ -16,9 +16,72 @@
 6. LOD
 4. 材质导出
 
-# 编译注意
+## 编译注意
 1. 链接库
 	1. 动态 Libfbxsdk.lib, fbxsdk-<version>.dll ，__注意__:编译时需要带`FBXSDK_SHARED`宏
 	2. 静态
 		1.  `/MT`: LIBCMT.lib静态链接
 		2.  `/MD`: LIBCMT.lib动态链接
+
+## 映射方式
+决定每个元素怎么映射到一个平面上  
+
+	enum EMappingMode
+	{
+		eNone,
+		eByControlPoint,
+		eByPolygonVertex,
+		eByPolygon,
+		eByEdge,
+		eAllSame
+	};
+1. eNone：The mapping is undetermined.
+2. __eByControlPoint__: There will be one mapping coordinate for each surface control point/vertex.
+3. __eByPolygonVertex__:There will be one mapping coordinate for each vertex, for every polygon of which it is a part.This means that a vertex will have as many mapping coordinates as polygons of which it is a part. 
+4. eByPolygon: There can be only one mapping coordinate for the whole polygon.
+5. eByEdge:   There will be one mapping coordinate for each unique edge in the mesh.This is meant to be used with **smoothing layer elements**.
+6. eAllSame:  There can be only one mapping coordinate for the whole surface.
+
+## 索引方式
+Determines how the mapping information is stored in the array of coordinates. 
+
+	enum EReferenceMode
+	{
+		eDirect,
+		eIndex,
+		eIndexToDirect
+	};
+
+1. **eDirect**: This indicates that the mapping information for the n'th element is found in the n'th place of `FbxLayerElementTemplate::mDirectArray`.
+2. eIdex: This symbol is kept for backward compatibility with FBX v5.0 files. In FBX v6.0 and higher,this symbol is replaced with eIndexToDirect.
+3. **eIndexToDirect**:This indicates that the `FbxLayerElementTemplate::mIndexArray` contains, for the n'th element, an **index** in the `FbxLayerElementTemplate::mDirectArray` array of mapping elements. eIndexToDirect is usually useful for storing **eByPolygonVertex mapping mode** elements coordinates. Since the same coordinates are usually repeated many times, this **saves spaces** by storing the coordinate only one time and then referring to them with an index. Materials and Textures are also referenced with this mode and the actual Material/Texture can be accessed via the FbxLayerElementTemplate::mDirectArray.
+
+## Geometry Element
+
+	/**  Defines geometry element classes.
+	  *  A geometry element describes how the geometry element is mapped to a geometry surface
+	  *  and how the mapping information is arranged in memory.
+	  *  \remarks Geometry elements are independent of layer elements and hide the complexity of layers.
+	  *  \code
+	  *  FbxGeometryElementUV* lUVs = lMesh->GetElementUV("map1");
+	  *  FbxGeometryElementUV::DirectArrayType lDirectArray = lUVs->GetDirectArray();
+	  *  int lDirectUVCount = lDirectArray.GetCount();
+	  *  FbxVector2 lFirstUV = lDirectArray[0];
+	  *  \endcode
+	  *  \see FbxGeometryBase
+	  */
+
+typedef FbxLayerElement FbxGeometryElement;
+typedef FbxLayerElementNormal FbxGeometryElementNormal;
+typedef FbxLayerElementBinormal FbxGeometryElementBinormal;
+typedef FbxLayerElementTangent FbxGeometryElementTangent;
+typedef FbxLayerElementMaterial FbxGeometryElementMaterial;
+typedef FbxLayerElementPolygonGroup FbxGeometryElementPolygonGroup;
+typedef FbxLayerElementUV FbxGeometryElementUV;
+typedef FbxLayerElementVertexColor FbxGeometryElementVertexColor;
+typedef FbxLayerElementUserData FbxGeometryElementUserData;
+typedef FbxLayerElementSmoothing FbxGeometryElementSmoothing;
+typedef FbxLayerElementCrease FbxGeometryElementCrease;
+typedef FbxLayerElementHole FbxGeometryElementHole;
+typedef FbxLayerElementVisibility FbxGeometryElementVisibility
+
--- Engine/Source/FbxResearch/Private/Main.cpp
+++ Engine/Source/FbxResearch/Private/Main.cpp
@@ -3,6 +3,7 @@
 #include <fbxsdk.h>
 #include <iostream>
 #include "FbxCommon.h"
+#include "fbxsdk\scene\geometry\fbxlayer.h"
 class ResearchFbx : public FDefaultModuleImpl
 {
 public:
@@ -44,6 +45,196 @@ void CreateTexture(FbxScene* pScene)
 	gTexture->SetScale(1.0, 1.0);
 	gTexture->SetRotation(0.0, 0.0);
 }
+
+void CreateMaterial(FbxScene* pScene)
+{
+	FbxString lMaterialName = "material";
+	FbxString lShadingName = "Phong";
+	FbxDouble3 lBlack(0.0, 0.0, 0.0);
+	FbxDouble3 lRed(1.0, 0.0, 0.0);
+	FbxDouble3 lDiffuseColor(0.75, 0.75, 0.0);
+	gMaterial = FbxSurfacePhong::Create(pScene, lMaterialName.Buffer());
+
+	// Generate primary and secondary colors.
+	gMaterial->Emissive.Set(lBlack);
+	gMaterial->Ambient.Set(lRed);
+	gMaterial->Diffuse.Set(lDiffuseColor);
+	gMaterial->TransparencyFactor.Set(40.5);
+	gMaterial->ShadingModel.Set(lShadingName);
+	gMaterial->Shininess.Set(0.5);
+
+	// the texture need to be connected to the material on the corresponding property
+	if (gTexture)
+		gMaterial->Diffuse.ConnectSrcObject(gTexture);
+}
+
+
+void AddMaterials(FbxMesh* pMesh)
+{
+	// Set material mapping.
+	FbxGeometryElementMaterial* lMaterialElement = pMesh->CreateElementMaterial();
+	lMaterialElement->SetMappingMode(FbxGeometryElement::eByPolygon);
+	lMaterialElement->SetReferenceMode(FbxGeometryElement::eIndexToDirect);
+
+	//get the node of mesh, add material for it.
+	FbxNode* lNode = pMesh->GetNode();
+	if (lNode == NULL)
+		return;
+	lNode->AddMaterial(gMaterial);
+
+	// We are in eByPolygon, so there's only need for 6 index (a cube has 6 polygons).
+	lMaterialElement->GetIndexArray().SetCount(6);
+
+	// Set the Index 0 to 6 to the material in position 0 of the direct array.
+	for (int i = 0; i < 6; ++i)
+		lMaterialElement->GetIndexArray().SetAt(i, 0);
+}
+
+
+FbxNode* CreateCubeMesh(FbxScene* pScene, char* pName)
+{
+	int i, j;
+	FbxMesh* lMesh = FbxMesh::Create(pScene, pName);
+
+	FbxVector4 lControlPoint0(-50, 0, 50);
+	FbxVector4 lControlPoint1(50, 0, 50);
+	FbxVector4 lControlPoint2(50, 100, 50);
+	FbxVector4 lControlPoint3(-50, 100, 50);
+	FbxVector4 lControlPoint4(-50, 0, -50);
+	FbxVector4 lControlPoint5(50, 0, -50);
+	FbxVector4 lControlPoint6(50, 100, -50);
+	FbxVector4 lControlPoint7(-50, 100, -50);
+
+	FbxVector4 lNormalXPos(1, 0, 0);
+	FbxVector4 lNormalXNeg(-1, 0, 0);
+	FbxVector4 lNormalYPos(0, 1, 0);
+	FbxVector4 lNormalYNeg(0, -1, 0);
+	FbxVector4 lNormalZPos(0, 0, 1);
+	FbxVector4 lNormalZNeg(0, 0, -1);
+
+	// Create control points.
+	lMesh->InitControlPoints(24);
+	FbxVector4* lControlPoints = lMesh->GetControlPoints();
+
+	lControlPoints[0] = lControlPoint0;
+	lControlPoints[1] = lControlPoint1;
+	lControlPoints[2] = lControlPoint2;
+	lControlPoints[3] = lControlPoint3;
+	lControlPoints[4] = lControlPoint1;
+	lControlPoints[5] = lControlPoint5;
+	lControlPoints[6] = lControlPoint6;
+	lControlPoints[7] = lControlPoint2;
+	lControlPoints[8] = lControlPoint5;
+	lControlPoints[9] = lControlPoint4;
+	lControlPoints[10] = lControlPoint7;
+	lControlPoints[11] = lControlPoint6;
+	lControlPoints[12] = lControlPoint4;
+	lControlPoints[13] = lControlPoint0;
+	lControlPoints[14] = lControlPoint3;
+	lControlPoints[15] = lControlPoint7;
+	lControlPoints[16] = lControlPoint3;
+	lControlPoints[17] = lControlPoint2;
+	lControlPoints[18] = lControlPoint6;
+	lControlPoints[19] = lControlPoint7;
+	lControlPoints[20] = lControlPoint1;
+	lControlPoints[21] = lControlPoint0;
+	lControlPoints[22] = lControlPoint4;
+	lControlPoints[23] = lControlPoint5;
+
+	// We want to have one normal for each vertex (or control point),
+	// so we set the mapping mode to eByControlPoint.
+	FbxGeometryElementNormal* lGeometryElementNormal = lMesh->CreateElementNormal();
+
+	lGeometryElementNormal->SetMappingMode(FbxGeometryElement::eByControlPoint);
+
+	// Set the normal values for every control point.
+	lGeometryElementNormal->SetReferenceMode(FbxGeometryElement::eDirect);
+
+	lGeometryElementNormal->GetDirectArray().Add(lNormalZPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalZPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalZPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalZPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalXPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalXPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalXPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalXPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalZNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalZNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalZNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalZNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalXNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalXNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalXNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalXNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalYPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalYPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalYPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalYPos);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalYNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalYNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalYNeg);
+	lGeometryElementNormal->GetDirectArray().Add(lNormalYNeg);
+
+
+	// Array of polygon vertices.
+	int lPolygonVertices[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12, 13,
+		14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
+
+
+	// Create UV for Diffuse channel.
+	FbxGeometryElementUV* lUVDiffuseElement = lMesh->CreateElementUV("DiffuseUV");
+	FBX_ASSERT(lUVDiffuseElement != NULL);
+	lUVDiffuseElement->SetMappingMode(FbxGeometryElement::eByPolygonVertex);
+	lUVDiffuseElement->SetReferenceMode(FbxGeometryElement::eIndexToDirect);
+
+	FbxVector2 lVectors0(0, 0);
+	FbxVector2 lVectors1(1, 0);
+	FbxVector2 lVectors2(1, 1);
+	FbxVector2 lVectors3(0, 1);
+
+	lUVDiffuseElement->GetDirectArray().Add(lVectors0);
+	lUVDiffuseElement->GetDirectArray().Add(lVectors1);
+	lUVDiffuseElement->GetDirectArray().Add(lVectors2);
+	lUVDiffuseElement->GetDirectArray().Add(lVectors3);
+
+	//Now we have set the UVs as eIndexToDirect reference and in eByPolygonVertex  mapping mode
+	//we must update the size of the index array.
+	lUVDiffuseElement->GetIndexArray().SetCount(24);
+
+	// Create polygons. Assign texture and texture UV indices.
+	for (i = 0; i < 6; i++)
+	{
+		// all faces of the cube have the same texture
+		lMesh->BeginPolygon(-1, -1, -1, false);
+
+		for (j = 0; j < 4; j++)
+		{
+			// Control point index
+			lMesh->AddPolygon(lPolygonVertices[i * 4 + j]);
+
+			// update the index array of the UVs that map the texture to the face
+			lUVDiffuseElement->GetIndexArray().SetAt(i * 4 + j, j);
+		}
+
+		lMesh->EndPolygon();
+	}
+
+	// create a FbxNode
+	FbxNode* lNode = FbxNode::Create(pScene, pName);
+
+	// set the node attribute
+	lNode->SetNodeAttribute(lMesh);
+
+	// set the shading mode to view texture
+	lNode->SetShadingMode(FbxNode::eTextureShading);
+
+	// rescale the cube
+	lNode->LclScaling.Set(FbxVector4(0.3, 0.3, 0.3));
+
+	// return the FbxNode
+	return lNode;
+}
+
 int main(int argc,TCHAR* argv[])
 {
 	FCommandLine::Set(TEXT(""));
