769d1dcff487d300899c60de07193b768f5d4f22
yearling <yearlingWES@gmail.com>
2017-09-05 15:09:29 +0800

    remove mutiscreen

--- Engine/Source/Experimental/Private/game_main.cpp
+++ Engine/Source/Experimental/Private/game_main.cpp
@@ -26,20 +26,30 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	GLogConsole->Show(true);
 	FPlatformOutputDevices::SetupOutputDevices();
 	FPlatformMemory::Init();
-	const FPlatformMemoryStats& StateReport = FPlatformMemory::GetStats();
-	std::cout << "MemoryTotalPhysical: " << StateReport.TotalPhysicalGB << " GB" << std::endl;
-	std::cout << "MemoryTotalVirtual: " << (StateReport.TotalVirtual / 1024 / 1024 / 1024) << " GB" << std::endl;
-	std::cout << "MemoryPage: " << StateReport.PageSize / 1024 << " KB" << std::endl;
-
 	FMallocLeakDetection::Get().SetAllocationCollection(true);
+	GLogConsole->Logf__VA(TEXT("log's name %s"), *FPlatformOutputDevices::GetAbsoluteLogFilename());
+
+	FILE *stream;
+	freopen_s(&stream, "CONOUT$", "w", stdout);
+	freopen_s(&stream, "CONIN$", "w+t", stdin);
+	freopen_s(&stream, "CONOUT$", "w+t", stderr);
+	setlocale(LC_ALL, "chs");
+	std::ios::sync_with_stdio();
+	std::cout.clear();//防止在AllocConsole之前调用cout，这样子的话，cout.badbit set会被设置，然后就什么也不能输出了。
+	//见http://stackoverflow.com/questions/18914506/doing-a-stdendl-before-allocconsole-causes-no-display-of-stdcout
+	std::wcout.clear();
 	int result=0;
 	YYUTApplication* yy_app=GetApplication();
 	yy_app->Initial();
+	//std::wcout << "hello" << std::endl;
 	result=yy_app->Run();
 	yy_app->Exit();
 	FMallocLeakDetection::Get().SetAllocationCollection(false);
 	FMallocLeakDetection::Get().DumpPotentialLeakers();
 	FMallocLeakDetection::Get().DumpOpenCallstacks();
-	GLog->TearDown();
+	if (GLog)
+	{
+		GLog->TearDown();
+	}
 	return result;
 }
